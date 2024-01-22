23c88c6da3dfa3d27311980519938c78dae76557
Yezir971 <james_ahmedaly@yahoo.com>
2024-01-22 16:02:17 +0100

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function(){
+    let button = document.getElementsByTagName('button');
+    let size = button.length;
+    for(let i; i<size ; i++){
+        // button[i]
+    }
     console.log('gg');
 });
